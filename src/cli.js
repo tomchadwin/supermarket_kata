d704d49f320c5d6c5234c589fcf898309f13f83d
tomchadwinAstun <tomchadwin@astuntechnology.com>
2023-05-18 23:37:53 +0100

    Add categories, add more products, tidy

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,64 @@
+let cart = [];
+
 class item {
-  constructor(name='unknown', price=0, model='quantity') {
-    this.name = name;
-    this.price = price;
-    this.model = model;
+  constructor(props) {
+    this.id = props.id;
+    this.name = props.name;
+    this.price = props.price;
+    this.model = props.model;
+  }
+};
+
+let beans = new item({
+  id: 1, 
+  name: 'Baked beans', 
+  price: .99,
+  model: 'quantity'
+});
+let cola = new item({
+  id: 2,
+  name: 'Can of cola', 
+  price: .75,
+  model: 'quantity'
+});
+let onions = new item({
+  id: 3,
+  name: 'Onions', 
+  price: .49,
+  model: 'kilo'
+});
+let red_rowan = new item({
+  id: 4,
+  name: 'Red Rowan ale', 
+  price: 2.99,
+  model: 'item'
+});
+let reiver = new item({
+  id: 5,
+  name: 'Reiver bitter', 
+  price: 2.99,
+  model: 'item'
+});
+let stell = new item({
+  id: 6,
+  name: 'Stell stout', 
+  price: 2.99,
+  model: 'item'
+});
+
+class category {
+  constructor(props) {
+    this.id = props.id;
+    this.description = props.description;
   }
 }
 
-let beans = new item('Baked beans', .99, 'quantity');
-let cola = new item('Can of cola', .75, 'quantity');
-let onions = new item('Onions', .49, 'kilo');
+let beers = new category({
+  id: 1,
+  description: 'Beer',
+  items : [red_rowan, reiver, stell]
+});
+
 
 export function cli(args) {
   console.log(getArgs(args));
